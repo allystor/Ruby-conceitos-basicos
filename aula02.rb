4c795bdace732ff3253f2ea35cd7813e343fbbb3
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-03-12 22:44:47 -0300

    update commit

---
+++ aula02.rb
@@ -0,0 +1,37 @@
+#Assim como em qualquer linguagem de programação, os operadores lógiocos também funcionam da mesma forma em Ruby os operadores numéricos como os de adição(+),subtração(-), multiplicação(*),divisão(/) Exemplo:
+
+#somando:
+a = gets.chomp.to_i
+puts "Digite o primeiro valor: "
+b = gets.chomp.to_i
+puts "Agora digte o segundo valor: "
+
+soma = a + b
+puts " a soma de #{a} com #{b} é igual a: #{soma}"
+
+#subtraindo:
+c = gets.chomp.to_i
+puts "Digite o primeiro valor: "
+d = gets.chomp.to_i
+puts "Agora digte o segundo valor: "
+
+subtração = c - d
+puts " a sutração de #{c} com #{d} é igual a: #{subtração}"
+
+#multiplicando:
+e = gets.chomp.to_i
+puts "Digite o primeiro valor: "
+f = gets.chomp.to_i
+puts "Agora digte o segundo valor: "
+
+multiplicação = e * f
+puts " a multiplicação de #{e} com #{f} é igual a: #{multiplicação}"
+
+#dividindo:
+g = gets.chomp.to_i
+puts "Digite o primeiro valor: "
+h = gets.chomp.to_i
+puts "Agora digte o segundo valor: "
+
+divisão = g / h 
+puts " a divisão de #{g} com #{h} é igual a: #{divisão}"
