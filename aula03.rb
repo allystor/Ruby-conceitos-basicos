3b319a58ee589050db20d1167c84325aaf366393
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-03-13 18:47:15 -0300

    update commit

---
+++ aula03.rb
@@ -0,0 +1,16 @@
+#Também em Ruby. possuimos a tabela da verdade onde são aplicado as estruturas condicionais com valores de verdadeiro ou falso como apresentados abaixo.        
+
+#criamos uma variável x com o valor 20 em e ocorrerá os seguintes eventos:
+
+#declaração da variável.
+x = 20
+#Se(if) o valor da variável realmente for 10 elaé uma variável do tipo verdadeira(True) ira declarar a mensagem abaixo.
+if x == 20
+    puts"x é igual a 20"
+#Se não se(elsif) ele irá realaizar a operação do código para achar um novo valor verdadeiro(true). 
+elsif x == 70
+    puts"x é igual a 70"
+#Se não(else) a variável é falsa(false) e imprimirar a mensagem abaixo.
+else
+    puts"x não é igual a 20"
+end
